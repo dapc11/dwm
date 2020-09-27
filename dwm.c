e0a80e916707123575b4915ce8c44e6a17696bcd
Daniel Tingelöf <daniel.tingelof@gmail.com>
2020-09-27 21:28:23 +0200

    Colors

--- dwm.c
+++ dwm.c
@@ -1356,7 +1356,7 @@ loadxrdb()
 
       if (xrdb != NULL) {
           XRDB_LOAD_COLOR("*color0", normbordercolor);
-          XRDB_LOAD_COLOR("*color7", selbordercolor);
+          XRDB_LOAD_COLOR("*color8", selbordercolor);
           XRDB_LOAD_COLOR("*color0", normbgcolor);
           XRDB_LOAD_COLOR("*color8", normfgcolor);
           XRDB_LOAD_COLOR("*color0", selfgcolor);
