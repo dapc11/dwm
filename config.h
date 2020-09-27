ee10b4cd57a5583ff974ff77bb5eaa24bbeac9cc
Daniel Tingelöf <daniel.tingelof@gmail.com>
2020-09-27 19:21:01 +0200

    hack and rofi shortcut change

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "hack:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char dmenufont[]             = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -105,7 +105,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 /*static const char *termcmd[]  = { "st", NULL };*/
-static const char *termcmd[]  = { "xterm", NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
 
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
@@ -159,7 +159,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,         XK_Return,          togglescratch,  {.ui = 0} },
     { MODKEY,                   XK_z,               incrgaps,       {.i = +3 } },
     { MODKEY,                   XK_x,               incrgaps,       {.i = -3 } },
-    { MODKEY,                   XK_b,               spawn,          SHCMD("rofi -show window") },
+    { MODKEY,                   XK_s,               spawn,          SHCMD("rofi -show window") },
     { MODKEY,                   XK_d,               spawn,          SHCMD("rofi -show drun -display-drun "" -modi drun") },
     { MODKEY|ControlMask,       XK_Left,            focusmon,       {.i = -1 } },
     { MODKEY|ShiftMask,         XK_Left,            tagmon,         {.i = -1 } },
